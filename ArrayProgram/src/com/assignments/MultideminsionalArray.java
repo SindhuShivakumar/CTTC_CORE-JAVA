b46d0ab2a28c9661d49dcfee99d1e8571adcd95f
lenovo <lenovo@BYOD-661564>
2023-01-11 10:45:23 +0530

    FIRST COMMIT

---
+++ ArrayProgram/src/com/assignments/MultideminsionalArray.java
@@ -0,0 +1,9 @@
+package com.assignments;
+
+public class MultideminsionalArray {
+	public static void main(String[] args) {
+		  
+		   int arr[][][] = {{{ 4,5,5}, {1,2,3}}, {{3,4,5},{2,3,5}}};
+		   System.out.println(arr[0][1][0]);
+	}
+}
---
+++ ArrayProgram/src/com/assignments/TwoDimensionalarray.java
@@ -0,0 +1,10 @@
+package com.assignments;
+
+public class TwoDimensionalarray {
+	public static void main(String[] args) {
+		  
+		   int arr[][] = {{ 4,5,5}, {1,2,3}, {3,4,5}};
+		   System.out.println(arr[0][1]);
+	}		   
+
+}
